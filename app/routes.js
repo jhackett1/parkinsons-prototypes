9912c8bf132797d7a67d5477612eaa2817159529
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-07-02 12:58:27 +0100

    medicine reminder saves data

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,16 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.post("/v1/medicine-reminders", (req, res)=>{
+    if(req.session.data.reminders){
+        let reminders = req.session.data.reminders
+        reminders.push(req.body)
+    } else {
+        req.session.data.reminders = [{
+            ...req.body
+        }]
+    }
+    res.redirect("/v1/medicine-reminders/index")
+})
+
 module.exports = router
